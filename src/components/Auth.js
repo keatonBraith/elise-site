92ef11a5fd8f536f3058e1a7b7992e44f3e9a64a
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-01-06 23:19:45 -0800

    home page styling

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from "react";
+import './Auth.css';
 
 const Auth = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [newUser, setNewUser] = useState(false)
+  const [newUser, setNewUser] = useState(false);
 
-    return (
-        <div>
-            Auth
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      <div className='main--auth'>
+        <h1>Signup</h1>
+      </div>
+    </Fragment>
+  );
+};
 
-export default Auth
+export default Auth;
