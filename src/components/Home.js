d8f76978bc9c7a5d746b466b87c9ca66fbb1b20b
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-01-14 09:15:01 -0800

    Added some cards on the categories section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { Card, Button, Row, Col, Container } from "react-bootstrap";
 // import "./Home.css";
 
 const Home = () => {
@@ -21,12 +22,59 @@ const Home = () => {
       <div className="home--categories">
         <h2 className="display-3">Find what fascinates you</h2>
         <h4>Animation, Design, Illustration, Lifestyle, Photos, Business</h4>
-        <div className="cards">
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-        </div>
+        <Container>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1475669698648-2f144fcaaeb1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGRlc2lnbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                />
+                <Card.Body>
+                  <Card.Title className='text-center'>ANIMATION</Card.Title>
+                  <Card.Text>Text to build on</Card.Text>
+                  <Button className='text-center' variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+            <Card className='text-center' style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1541462608143-67571c6738dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
+          <Card.Body>
+            <Card.Title>DESIGN</Card.Title>
+            <Card.Text>
+              Text to build on
+            </Card.Text>
+            <Button className='text-center' variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+            </Col>
+            <Col>
+            <Card className='text-center' style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1597809259188-0e5ffcbb0ba9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
+          <Card.Body>
+            <Card.Title>ILLUSTRATION</Card.Title>
+            <Card.Text>
+              Text to build on
+            </Card.Text>
+            <Button className='text-center' variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+            </Col>
+            <Col>
+            <Card className='text-center' style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG9ncmFwaHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" />
+          <Card.Body>
+            <Card.Title>PHOTOS</Card.Title>
+            <Card.Text>
+              Text to build on
+            </Card.Text>
+            <Button className='text-center' variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
   );
