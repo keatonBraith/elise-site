59c0440f32694d3ff7d1c07c0c2d3cb71db2050c
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-01-14 08:33:10 -0800

    fixed routes that werent working

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import React from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Auth from './components/Auth';
 import Search from './components/Search';
+import { Switch, Route } from 'react-router-dom'
 
 
 export default (
-    <Router>
+    <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/auth" component={Auth} />
         <Route path="/about" component={About} />
         <Route path="/search" component={Search} />
-    </Router>
+    </Switch>
 )
 
 
